558d6a9b1852f9190978ab45a5b175bdc4ab413d
jacob <jacob@ycnrg.org>
2015-10-07 15:50:30 -0400

    added setuptools tooling

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+setup(
+    name = "yc_xbake",
+    version = "0.10.1",
+    author = "Jacob Hipps",
+    author_email = "jacob@ycnrg.org",
+    license = "GPL",
+    description = "Tool for cataloging and transcoding video files",
+    keywords = "video scraper scanner catalog subtitles",
+    url = "https://bitbucket.org/yellowcrescent/yc_xbake/",
+
+    packages = find_packages(),
+    scripts = ['yc_xbake'],
+
+    install_requires = ['docutils>=0.3','pymongo>=3.0','redis>=2.10'],
+
+    package_data = {
+        '': [ '*.md' ],
+    }
+
+    # could also include long_description, download_url, classifiers, etc.
+)
