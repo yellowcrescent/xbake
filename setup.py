3f419a10b209497be100b11ad81cd16004297ef0
Jacob Hipps <jacob@ycnrg.org>
2015-12-23 15:58:17 -0500

    modified parse_cli() to allow infile and outfile to be specified as positional args (without -i and -o flags)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "yc_xbake",
-    version = "0.10.153",
+    version = "0.10.16",
     author = "Jacob Hipps",
     author_email = "jacob@ycnrg.org",
     license = "MIT",
